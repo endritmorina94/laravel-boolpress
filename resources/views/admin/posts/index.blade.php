d7dc90d269d4dff148aea1e0182437ac536eca33
endritmorina94 <endrit.morina@outlook.it>
2021-06-18 15:03:52 +0200

    creata migrate e quindi crata la table nel db

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Ciao sono l'index admin dei post</h1>
+@endsection
---
+++ resources/views/guest/posts/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('content')
+    <h1>Ciao sono l'index pubblica dei post</h1>
+@endsection

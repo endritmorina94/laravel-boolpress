1509d7bd955afa2b1ad81d294701bdf78f69ab36
endritmorina94 <endrit.morina@outlook.it>
2021-06-18 15:11:44 +0200

    creato seeder e quindi popolata la table del DB

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    //Specifico quali sono le colonne che possono essere modificate in automatico con la funzione fill()
+    protected $fillable = [
+      'title',
+      'content',
+      'slug'
+  ];
 }
